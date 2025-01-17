75a396f9d181354e783e3c8e31b9a79840dc4151
Ryan Kendall <ryankendallt@gmail.com>
2025-01-17 10:36:18 +0000

    Fix uncaught exceptions (#14)

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,12 +1,20 @@
-import { Router } from 'express'
+import { RequestHandler, Router } from 'express'
 import { Services } from '../services'
 import * as localDeliveryUnitMailboxes from './localDeliveryUnitMailboxes/controller'
+import asyncMiddleware from '../middleware/asyncMiddleware'
 
-export default (services: Services): Router =>
-  Router()
-    .get('/', async (req, res, next) => res.render('pages/index'))
-    .get('/local-delivery-unit-mailboxes', localDeliveryUnitMailboxes.index(services))
-    .get('/local-delivery-unit-mailboxes/new', localDeliveryUnitMailboxes.newMailbox)
-    .post('/local-delivery-unit-mailboxes', localDeliveryUnitMailboxes.create(services))
-    .get('/local-delivery-unit-mailboxes/:id/edit', localDeliveryUnitMailboxes.edit(services))
-    .post('/local-delivery-unit-mailboxes/:id', localDeliveryUnitMailboxes.update(services))
+export default (services: Services): Router => {
+  const router = Router()
+
+  const get = (path: string, ...handlers: RequestHandler[]) => router.get(path, handlers.map(asyncMiddleware))
+  const post = (path: string, ...handlers: RequestHandler[]) => router.post(path, handlers.map(asyncMiddleware))
+
+  get('/', async (req, res, next) => res.render('pages/index'))
+  get('/local-delivery-unit-mailboxes', localDeliveryUnitMailboxes.index(services))
+  get('/local-delivery-unit-mailboxes/new', localDeliveryUnitMailboxes.newMailbox)
+  post('/local-delivery-unit-mailboxes', localDeliveryUnitMailboxes.create(services))
+  get('/local-delivery-unit-mailboxes/:id/edit', localDeliveryUnitMailboxes.edit(services))
+  post('/local-delivery-unit-mailboxes/:id', localDeliveryUnitMailboxes.update(services))
+
+  return router
+}
