22c22b7bdee0e522a0f73a4f014a4c9bef9cbcdf
Ryan Kendall <ryankendallt@gmail.com>
2025-01-21 09:59:09 +0000

    Front and backend validations can be handled (#18)
    * Front and backend validations can be handled
    
    * style tweak

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -13,7 +13,7 @@ export default (services: Services): Router => {
   get('/', async (req, res, next) => res.render('pages/index'))
   get('/local-delivery-unit-mailboxes', localDeliveryUnitMailboxes.index(services))
   get('/local-delivery-unit-mailboxes/new', localDeliveryUnitMailboxes.newMailbox)
-  post('/local-delivery-unit-mailboxes', localDeliveryUnitMailboxes.create(services))
+  post('/local-delivery-unit-mailboxes', ...localDeliveryUnitMailboxes.create(services))
   get('/local-delivery-unit-mailboxes/:id/edit', localDeliveryUnitMailboxes.edit(services))
   post('/local-delivery-unit-mailboxes/:id', localDeliveryUnitMailboxes.update(services))
   get('/local-delivery-unit-mailboxes/:id/delete', localDeliveryUnitMailboxes.confirmDelete(services))
