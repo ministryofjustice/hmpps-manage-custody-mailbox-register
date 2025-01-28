f0d528a46ceec180e4909890e4f6bcb1ddea7015
Ryan Kendall <ryankendallt@gmail.com>
2025-01-28 14:15:51 +0000

    mroe tests for LDU create

--- server/routes/localDeliveryUnitMailboxes/controller.test.ts
+++ server/routes/localDeliveryUnitMailboxes/controller.test.ts
@@ -6,6 +6,9 @@ describe('create', () => {
   const mailboxRegisterService = { createLocalDeliveryUnitMailbox: jest.fn() }
   const services = { mailboxRegisterService } as unknown as Services
   const flash: Record<string, string> = {}
+  const flasher = (name: string, message: string) => {
+    flash[name] = message
+  }
 
   it.each([
     ['emailAddress', 'notValid', 'Please enter a valid email address'],
@@ -22,18 +25,30 @@ describe('create', () => {
       [field]: value,
     }
 
-    const req = {
-      params: {},
-      body,
-      flash: (name: string, message: string) => {
-        flash[name] = message
-      },
-    } as unknown as Request
+    const req = { params: {}, body, flash: flasher } as unknown as Request
     const res = { redirect: jest.fn(), render: jest.fn() } as unknown as Response
     const next = jest.fn()
     await create(services)(req, res, next)
 
     expect(res.redirect).toHaveBeenCalledWith('/local-delivery-unit-mailboxes/new')
     expect(flash.validationErrors).toEqual(JSON.stringify({ [field]: expectedMessage }))
+  })
+
+  it('sends the form data to the back end', async () => {
+    const body = {
+      name: 'A Name',
+      emailAddress: 'valid@email.com',
+      country: 'A Country',
+      unitCode: '123',
+      areaCode: 'ABC',
+    }
+
+    const req = { body, middleware: { clientToken: 'CL13NT_T0K3N' } } as unknown as Request
+    const res = { redirect: jest.fn() } as unknown as Response
+    const next = jest.fn()
+
+    await create(services)(req, res, next)
+
+    expect(mailboxRegisterService.createLocalDeliveryUnitMailbox).toHaveBeenCalledWith('CL13NT_T0K3N', body)
   })
 })
