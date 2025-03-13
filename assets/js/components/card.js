22c50e8f9361627666ca43e72d114fb9ae903962
Jesus Laiz <zheileman@users.noreply.github.com>
2025-03-13 10:03:07 +0000

    Ensure cards are clickable (#54)
    Small bit of JS to make the card component clickable as a whole, not only in their link.
    
    Raised by Andy. Using suggested code.

---
+++ assets/js/components/card.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  initCards()
+})
+
+const initCards = () => {
+  // Loops through dom and finds all elements with card--clickable class
+  document.querySelectorAll('.card--clickable').forEach(card => {
+    // Check if card has a link within it
+    if (card.querySelector('a') !== null) {
+      // Clicks the link within the heading to navigate to desired page
+      card.addEventListener('click', () => {
+        card.querySelector('a').click()
+      })
+    }
+  })
+}
