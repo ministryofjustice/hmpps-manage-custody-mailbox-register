c5905aba72fe0552414e209d1a76c2f8058333df
Jesus Laiz <zheileman@users.noreply.github.com>
2025-11-19 15:09:31 +0000

    chore: Bump dependencies (#83)
    And bring over some changes from `hmpps-template-typescript`.

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -5,7 +5,7 @@ export default configureAllowedScripts({
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     'node_modules/cypress@15.6.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/esbuild@0.25.12': 'ALLOW',
+    'node_modules/esbuild@0.27.0': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/protobufjs@7.5.4': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
