6f2c11a6ac4a5d3d15e6f086863fe3023792373d
Jesus Laiz <zheileman@users.noreply.github.com>
2025-12-15 11:27:13 +0000

    chore: Bump dependencies (#86)

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,9 +3,9 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
-    'node_modules/cypress@15.6.0': 'ALLOW',
+    'node_modules/cypress@15.7.1': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/esbuild@0.27.0': 'ALLOW',
+    'node_modules/esbuild@0.27.1': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/protobufjs@7.5.4': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
