ea143dbb6395c7491c8df65b4319d3147e3d2417
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2026-01-28 14:09:16 +0000

    Fix allowlist

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -7,7 +7,6 @@ export default configureAllowedScripts({
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/esbuild@0.27.2': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
-    'node_modules/protobufjs@7.5.4': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
 })
