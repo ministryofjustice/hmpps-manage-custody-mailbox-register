fd0cf9c6c70a6db80c0e2d73bd6138398feb9992
Jesus Laiz <zheileman@users.noreply.github.com>
2026-03-04 16:03:57 +0000

    MO-1910 Bump dependencies (#92)
    * MO-1910 Bump dependencies
    
    And general refresh of the project to align with some recent changes to the template repo.
    
    * Remove jQuery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,7 @@
-import $ from 'jquery'
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 
 import './components/card'
-
-window.$ = $
 
 govukFrontend.initAll()
 mojFrontend.initAll()
